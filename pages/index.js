cfe96fdcbb92595b6c72f3646ca0bafffc3999eb
Marcos Vinicius de lucena <60518425+MarcosViniicius@users.noreply.github.com>
2025-10-22 15:35:36 +0000

    adiciona script: `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
-function Home(){
-    return <h1>Manu, eu amo você! Se você me ama, dá uma risadinha.</h1>
+function Home() {
+  return <h1>Manu, eu amo você! Se você me ama, dá uma risadinha.</h1>;
 }
 
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("teste 2");
+}
 export default Home;
